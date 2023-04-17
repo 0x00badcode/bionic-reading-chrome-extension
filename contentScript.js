a67483a30b19006891786627e8f431143b14b0fc
0x00badcode <paul.crinquand@epitech.eu>
2023-04-17 10:42:32 +0200

    rewrote contentScript a bit + added styles

--- contentScript.js
+++ contentScript.js
@@ -1,61 +1,55 @@
-function applyEmphasisRules(word, shortWordsLength, mediumWordsLength, longWordsLength) {
-    const length = word.length;
-  
-    if (length <= 4) {
-      return `<span class="typoglycemia-bold">${word.slice(0, shortWordsLength)}</span>${word.slice(
-        shortWordsLength
-      )}`;
-    } else if (length <= 6) {
-      return `<span class="typoglycemia-bold">${word.slice(0, mediumWordsLength)}</span>${word.slice(
-        mediumWordsLength
-      )}`;
-    } else {
-      return `<span class="typoglycemia-bold">${word.slice(0, longWordsLength)}</span>${word.slice(
-        longWordsLength
-      )}`;
-    }
-  }
-  
-  function typoglycemiaTransform(text, shortWordsLength, mediumWordsLength, longWordsLength) {
-    return text.replace(/\b[a-zA-Z]+\b/g, (word) =>
-      applyEmphasisRules(word, shortWordsLength, mediumWordsLength, longWordsLength)
-    );
-  }
-  
-  function processNode(node, shortWordsLength, mediumWordsLength, longWordsLength) {
-    if (node.nodeType === Node.TEXT_NODE) {
-      const span = document.createElement('span');
-      span.innerHTML = typoglycemiaTransform(
-        node.textContent,
-        shortWordsLength,
-        mediumWordsLength,
-        longWordsLength
-      );
-      node.replaceWith(span);
-    } else if (node.nodeType === Node.ELEMENT_NODE) {
-      Array.from(node.childNodes).forEach((childNode) =>
-        processNode(childNode, shortWordsLength, mediumWordsLength, longWordsLength)
-      );
-    }
-  }
-  
-  function isGoogleSearchPage() {
-    const url = window.location.href;
-    const googleSearchPattern = /^https:\/\/(www\.)?google\..+\/search/;
-    return googleSearchPattern.test(url);
-  }
-  
-  chrome.storage.sync.get(
-    ['enabled', 'shortWordsLength', 'mediumWordsLength', 'longWordsLength'],
-    ({ enabled, shortWordsLength, mediumWordsLength, longWordsLength }) => {
-      if (enabled && !isGoogleSearchPage()) {
-        processNode(
-          document.body,
-          shortWordsLength || 1,
-          mediumWordsLength || 2,
-          longWordsLength || 3
-        );
+document.addEventListener('DOMContentLoaded', function () {
+
+    function applyEmphasisRules(word, shortWordsLength, mediumWordsLength, longWordsLength) {
+        const length = word.length;
+        const nonNumericPattern = /^[a-zA-Z]+$/;
+      
+        if (length <= 4 && nonNumericPattern.test(word)) {
+          return `<b>${word.slice(0, shortWordsLength)}</b>${word.slice(shortWordsLength)}`;
+        } else if (length <= 6 && nonNumericPattern.test(word)) {
+          return `<b>${word.slice(0, mediumWordsLength)}</b>${word.slice(mediumWordsLength)}`;
+        } else if (length > 6 && nonNumericPattern.test(word)) {
+          return `<b>${word.slice(0, longWordsLength)}</b>${word.slice(longWordsLength)}`;
+        } else {
+          return word;
+        }
       }
-    }
-  );
-  
+    
+    function processNode(node, shortWordsLength, mediumWordsLength, longWordsLength) {
+        if (node.nodeType === Node.TEXT_NODE) {
+          const words = node.textContent.split(/\b/);
+          for (let i = 0; i < words.length; i++) {
+            const word = words[i];
+            if (/^[a-zA-Z]+$/.test(word)) {
+              words[i] = applyEmphasisRules(word, shortWordsLength, mediumWordsLength, longWordsLength);
+            }
+          }
+          node.textContent = words.join('');
+        } else if (node.nodeType === Node.ELEMENT_NODE) {
+          Array.from(node.childNodes).forEach((childNode) =>
+            processNode(childNode, shortWordsLength, mediumWordsLength, longWordsLength)
+          );
+        }
+      }
+      
+      function isGoogleSearchPage() {
+        const url = window.location.href;
+        const googleSearchPattern = /^https:\/\/(www\.)?google\..+\/search/;
+        return googleSearchPattern.test(url);
+      }
+      
+      chrome.storage.sync.get(
+        {
+          shortWordsLength: 1,
+          mediumWordsLength: 2,
+          longWordsLength: 3,
+          extensionEnabled: true,
+        },
+        function(data) {
+          if (data.extensionEnabled && !isGoogleSearchPage()) {
+            processNode(document.body, data.shortWordsLength, data.mediumWordsLength, data.longWordsLength);
+          }
+        }
+      );
+      
+});
--- popup.js
+++ popup.js
@@ -1,37 +1,74 @@
-function saveOptions() {
-    const shortWordsLength = document.getElementById('shortWordsLength').value;
-    const mediumWordsLength = document.getElementById('mediumWordsLength').value;
-    const longWordsLength = document.getElementById('longWordsLength').value;
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the elements from the popup
+    const shortWordsLengthInput = document.getElementById('shortWordsLength');
+    const mediumWordsLengthInput = document.getElementById('mediumWordsLength');
+    const longWordsLengthInput = document.getElementById('longWordsLength');
+    const saveOptionsButton = document.getElementById('saveOptions');
+    const toggleExtensionInput = document.getElementById('toggleExtension');
   
-    chrome.storage.sync.set({
-      shortWordsLength,
-      mediumWordsLength,
-      longWordsLength,
-    });
-  }
-  
-  function loadOptions() {
+    // Load the options from storage and update the inputs
     chrome.storage.sync.get(
-      ['enabled', 'shortWordsLength', 'mediumWordsLength', 'longWordsLength'],
-      ({ enabled, shortWordsLength, mediumWordsLength, longWordsLength }) => {
-        document.getElementById('toggleExtension').checked = enabled;
-        document.getElementById('shortWordsLength').value = shortWordsLength || 1;
-        document.getElementById('mediumWordsLength').value = mediumWordsLength || 2;
-        document.getElementById('longWordsLength').value = longWordsLength || 3;
+      {
+        shortWordsLength: 3,
+        mediumWordsLength: 5,
+        longWordsLength: 7,
+        extensionEnabled: true,
+      },
+      function(data) {
+        shortWordsLengthInput.value = data.shortWordsLength;
+        mediumWordsLengthInput.value = data.mediumWordsLength;
+        longWordsLengthInput.value = data.longWordsLength;
+        toggleExtensionInput.checked = data.extensionEnabled;
+        toggleExtensionInput.addEventListener('change', function() {
+          chrome.storage.sync.set({
+            extensionEnabled: toggleExtensionInput.checked,
+          });
+          toggleExtensionInput.checked
+            ? enableExtension()
+            : disableExtension();
+        });
       }
     );
-  }
   
-  function toggleExtension() {
-    const enabled = document.getElementById('toggleExtension').checked;
-    chrome.storage.sync.set({ enabled }, () => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.reload(tabs[0].id);
-      });
+    // Save the options to storage when the Save button is clicked
+    saveOptionsButton.addEventListener('click', function() {
+      const shortWordsLength = parseInt(shortWordsLengthInput.value);
+      const mediumWordsLength = parseInt(mediumWordsLengthInput.value);
+      const longWordsLength = parseInt(longWordsLengthInput.value);
+      if (
+        shortWordsLength &&
+        mediumWordsLength &&
+        longWordsLength &&
+        shortWordsLength <= 4 &&
+        mediumWordsLength <= 6 &&
+        longWordsLength > 6
+      ) {
+        chrome.storage.sync.set({
+          shortWordsLength,
+          mediumWordsLength,
+          longWordsLength,
+        });
+      }
+    });
+  
+    // Enable or disable the extension based on the stored value
+    chrome.storage.sync.get({ extensionEnabled: true }, function(data) {
+      data.extensionEnabled ? enableExtension() : disableExtension();
+      toggleExtensionInput.checked = data.extensionEnabled;
+    });
+  });
+  
+  // Enable the extension by injecting a content script into the active tab
+  function enableExtension() {
+    chrome.tabs.executeScript({
+      file: 'content.js',
     });
   }
   
-  document.getElementById('saveOptions').addEventListener('click', saveOptions);
-  document.getElementById('toggleExtension').addEventListener('change', toggleExtension);
-  loadOptions();
+  // Disable the extension by removing the injected content script from the active tab
+  function disableExtension() {
+    chrome.tabs.executeScript({
+      code: 'document.querySelectorAll("b").forEach((el) => el.replaceWith(el.innerText))',
+    });
+  }
   
