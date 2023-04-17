b70d701ddd8ff6a0f9337a7dd3bdc9987fa095b2
0x00badcode <paul.crinquand@epitech.eu>
2023-04-17 10:25:08 +0200

    added options and a true toggle for the extension

--- contentScript.js
+++ contentScript.js
@@ -1,37 +1,61 @@
-function applyEmphasisRules(word) {
+function applyEmphasisRules(word, shortWordsLength, mediumWordsLength, longWordsLength) {
     const length = word.length;
-
+  
     if (length <= 4) {
-        return `<span class="typoglycemia-bold">${word.charAt(0)}</span>${word.slice(1)}`;
+      return `<span class="typoglycemia-bold">${word.slice(0, shortWordsLength)}</span>${word.slice(
+        shortWordsLength
+      )}`;
     } else if (length <= 6) {
-        return `<span class="typoglycemia-bold">${word.slice(0, 2)}</span>${word.slice(2)}`;
+      return `<span class="typoglycemia-bold">${word.slice(0, mediumWordsLength)}</span>${word.slice(
+        mediumWordsLength
+      )}`;
     } else {
-        return `<span class="typoglycemia-bold">${word.slice(0, 3)}</span>${word.slice(3)}`;
+      return `<span class="typoglycemia-bold">${word.slice(0, longWordsLength)}</span>${word.slice(
+        longWordsLength
+      )}`;
     }
-}
-
-function typoglycemiaTransform(text) {
-    return text.replace(/\b[a-zA-Z]+\b/g, applyEmphasisRules);
-}
-
-function processNode(node) {
+  }
+  
+  function typoglycemiaTransform(text, shortWordsLength, mediumWordsLength, longWordsLength) {
+    return text.replace(/\b[a-zA-Z]+\b/g, (word) =>
+      applyEmphasisRules(word, shortWordsLength, mediumWordsLength, longWordsLength)
+    );
+  }
+  
+  function processNode(node, shortWordsLength, mediumWordsLength, longWordsLength) {
     if (node.nodeType === Node.TEXT_NODE) {
-        const span = document.createElement('span');
-        span.innerHTML = typoglycemiaTransform(node.textContent);
-        node.replaceWith(span);
+      const span = document.createElement('span');
+      span.innerHTML = typoglycemiaTransform(
+        node.textContent,
+        shortWordsLength,
+        mediumWordsLength,
+        longWordsLength
+      );
+      node.replaceWith(span);
     } else if (node.nodeType === Node.ELEMENT_NODE) {
-        Array.from(node.childNodes).forEach(processNode);
+      Array.from(node.childNodes).forEach((childNode) =>
+        processNode(childNode, shortWordsLength, mediumWordsLength, longWordsLength)
+      );
     }
-}
-
-function isGoogleSearchPage() {
+  }
+  
+  function isGoogleSearchPage() {
     const url = window.location.href;
     const googleSearchPattern = /^https:\/\/(www\.)?google\..+\/search/;
     return googleSearchPattern.test(url);
-}
-
-chrome.storage.sync.get('enabled', ({ enabled }) => {
-    if (enabled && !isGoogleSearchPage()) {
-        processNode(document.body);
+  }
+  
+  chrome.storage.sync.get(
+    ['enabled', 'shortWordsLength', 'mediumWordsLength', 'longWordsLength'],
+    ({ enabled, shortWordsLength, mediumWordsLength, longWordsLength }) => {
+      if (enabled && !isGoogleSearchPage()) {
+        processNode(
+          document.body,
+          shortWordsLength || 1,
+          mediumWordsLength || 2,
+          longWordsLength || 3
+        );
+      }
     }
-});
+  );
+  
--- popup.js
+++ popup.js
@@ -1,7 +1,37 @@
-document.getElementById('toggleExtension').addEventListener('click', () => {
-    chrome.storage.sync.get('enabled', ({ enabled }) => {
-        chrome.storage.sync.set({ enabled: !enabled }, () => {
-            chrome.tabs.reload();
-        });
+function saveOptions() {
+    const shortWordsLength = document.getElementById('shortWordsLength').value;
+    const mediumWordsLength = document.getElementById('mediumWordsLength').value;
+    const longWordsLength = document.getElementById('longWordsLength').value;
+  
+    chrome.storage.sync.set({
+      shortWordsLength,
+      mediumWordsLength,
+      longWordsLength,
     });
-});
+  }
+  
+  function loadOptions() {
+    chrome.storage.sync.get(
+      ['enabled', 'shortWordsLength', 'mediumWordsLength', 'longWordsLength'],
+      ({ enabled, shortWordsLength, mediumWordsLength, longWordsLength }) => {
+        document.getElementById('toggleExtension').checked = enabled;
+        document.getElementById('shortWordsLength').value = shortWordsLength || 1;
+        document.getElementById('mediumWordsLength').value = mediumWordsLength || 2;
+        document.getElementById('longWordsLength').value = longWordsLength || 3;
+      }
+    );
+  }
+  
+  function toggleExtension() {
+    const enabled = document.getElementById('toggleExtension').checked;
+    chrome.storage.sync.set({ enabled }, () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.reload(tabs[0].id);
+      });
+    });
+  }
+  
+  document.getElementById('saveOptions').addEventListener('click', saveOptions);
+  document.getElementById('toggleExtension').addEventListener('change', toggleExtension);
+  loadOptions();
+  
