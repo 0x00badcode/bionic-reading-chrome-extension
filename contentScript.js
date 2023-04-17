ca40f364601e77f08b73d62e9fe2f5b9b64c27c8
0x00badcode <paul.crinquand@epitech.eu>
2023-04-17 10:02:59 +0200

    excluded front page of google

--- contentScript.js
+++ contentScript.js
@@ -1,32 +1,37 @@
 function applyEmphasisRules(word) {
     const length = word.length;
-  
+
     if (length <= 4) {
-      return `<span class="typoglycemia-bold">${word.charAt(0)}</span>${word.slice(1)}`;
+        return `<span class="typoglycemia-bold">${word.charAt(0)}</span>${word.slice(1)}`;
     } else if (length <= 6) {
-      return `<span class="typoglycemia-bold">${word.slice(0, 2)}</span>${word.slice(2)}`;
+        return `<span class="typoglycemia-bold">${word.slice(0, 2)}</span>${word.slice(2)}`;
     } else {
-      return `<span class="typoglycemia-bold">${word.slice(0, 3)}</span>${word.slice(3)}`;
+        return `<span class="typoglycemia-bold">${word.slice(0, 3)}</span>${word.slice(3)}`;
     }
-  }
-  
-  function typoglycemiaTransform(text) {
+}
+
+function typoglycemiaTransform(text) {
     return text.replace(/\b[a-zA-Z]+\b/g, applyEmphasisRules);
-  }
-  
-  function processNode(node) {
+}
+
+function processNode(node) {
     if (node.nodeType === Node.TEXT_NODE) {
-      const span = document.createElement('span');
-      span.innerHTML = typoglycemiaTransform(node.textContent);
-      node.replaceWith(span);
+        const span = document.createElement('span');
+        span.innerHTML = typoglycemiaTransform(node.textContent);
+        node.replaceWith(span);
     } else if (node.nodeType === Node.ELEMENT_NODE) {
-      Array.from(node.childNodes).forEach(processNode);
+        Array.from(node.childNodes).forEach(processNode);
     }
-  }
-  
-  chrome.storage.sync.get('enabled', ({ enabled }) => {
-    if (enabled) {
-      processNode(document.body);
+}
+
+function isGoogleSearchPage() {
+    const url = window.location.href;
+    const googleSearchPattern = /^https:\/\/(www\.)?google\..+\/search/;
+    return googleSearchPattern.test(url);
+}
+
+chrome.storage.sync.get('enabled', ({ enabled }) => {
+    if (enabled && !isGoogleSearchPage()) {
+        processNode(document.body);
     }
-  });
-  
+});
